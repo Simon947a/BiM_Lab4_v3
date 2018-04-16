f2ca8f8217b7da66ebc3969824a50c56090e7e2c
Simon94 <szymon7a@gmail.com>
2018-04-16 19:38:24 +0200

    .

---
+++ Assets/Scripts/CameraTrans.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrans : MonoBehaviour
+{
+
+    // Use this for initialization
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public GameObject player;
+
+    private Vector3 offset;
+
+    // Use this for initialization
+
+    void Start()
+
+    {
+
+        offset = transform.position - player.transform.position;
+
+    }
+
+    // Update is called once per frame
+
+    void LateUpdate()
+
+    {
+
+        transform.position = player.transform.position + offset;
+
+    }
+}
---
+++ Assets/Scripts/triger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triger : MonoBehaviour {
+
+    public GameObject wall;
+    private Animator anim;
+    void OnTriggerEnter(Collider other)
+    {
+        anim = wall.GetComponent<Animator>();
+        if (other.GetComponent<Collider>().CompareTag("Player"))
+        {
+           
+            anim.Play("animation");
+        }
+    }
+}
